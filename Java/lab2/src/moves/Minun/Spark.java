2c0d94449a1a996ed89a9d1f6302531c79f4416b
Badboychick <70016752+Badboychick@users.noreply.github.com>
2023-11-22 12:21:39 +0300

    Update Spark.java

--- Java/lab2/src/moves/Minun/Spark.java
+++ Java/lab2/src/moves/Minun/Spark.java
@@ -5,10 +5,4 @@ public class Spark extends PhysicalMove {
     public Spark(double pow, double acc) {
         super(Type.ELECTRIC, pow, acc);
     }
-
-    @Override
-    protected String describe() {
-        String[] rezhem = this.getClass().toString().split("\\.");
-        return "применяет " + rezhem[rezhem.length-1];
-    }
 }
