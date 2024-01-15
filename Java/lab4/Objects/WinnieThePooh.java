c02fbdf860f07a2b4a006a86c3856a8647a1890d
Badboychick <70016752+Badboychick@users.noreply.github.com>
2024-01-15 15:57:11 +0300

    Add files via upload

--- Java/lab4/Objects/WinnieThePooh.java
+++ Java/lab4/Objects/WinnieThePooh.java
@@ -5,7 +5,6 @@ import Enums.Gender;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.function.Consumer;
 
 import Exceptions.CannotDoThisAction;
 import Interfaces.Catchable;
@@ -13,16 +12,10 @@ public class WinnieThePooh extends Character implements Catchable {
     public ArrayList<BodyPart> wannaScratch = new ArrayList<>();
     public WinnieThePooh() {
         super("Винни Пух", Gender.Male);
-        this.wannaScratch = wannaScratch;
     }
     public String getBodyPartsToScratch() {
         final String[] result = {""};
-        this.wannaScratch.forEach(new Consumer<BodyPart>() {
-            @Override
-            public void accept(BodyPart part) {
-                result[0] += part.getName() + " ";
-            }
-        });
+        this.wannaScratch.forEach(part -> result[0] += part.getName() + " ");
         return result[0];
     }
     public void setWannaScratch(BodyPart bodyPart) {
@@ -33,14 +26,14 @@ public class WinnieThePooh extends Character implements Catchable {
         if (body_part.getCondition() == Boolean.FALSE) {
             System.out.println(getName() + " почесал " + body_part.getName());
             this.wannaScratch.remove(body_part);
-            field.set(body_part, (Boolean) Boolean.TRUE);
+            field.set(body_part, Boolean.TRUE);
             body_part.is_scratched = Boolean.TRUE;
         } else {
             System.out.println("Эта часть тела у Винни Пуха уже почесана. Сейчас он хочет почесать следующие части тела: " + this.getBodyPartsToScratch());
         }
     }
     @Override
-    public void grab(Character c, Trap trap) throws CannotDoThisAction{
+    public void grab(Character c, TrapSystem trap) throws CannotDoThisAction{
         if (this.hp > 0 && trap.isActive){
             System.out.println(this.getName() + " поймал " + c.getName() + " при помощи " + trap.getName() + "!");
             switch (trap.getType()) {
